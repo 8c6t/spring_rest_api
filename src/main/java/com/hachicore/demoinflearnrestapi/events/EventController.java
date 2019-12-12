860fbddc49fee5409c04399aa997e754bbd1843f
8c6t <nkw8620@gmail.com>
2019-12-13 02:56:10 +0900

    2.2 201 응답 받기

---
+++ src/main/java/com/hachicore/demoinflearnrestapi/events/EventController.java
@@ -0,0 +1,27 @@
+package com.hachicore.demoinflearnrestapi.events;
+
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@Controller
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
+public class EventController {
+
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody Event event) {
+        // ControllerLinkBuilder.linkTo -> WebMvcLinkBuilder.linkTo
+        // URI uri = linkTo(methodOn(EventController.class).createEvent(null)).slash("{id}").toUri();
+        URI createdUri = linkTo(EventController.class).slash("{id}").toUri();
+        event.setId(10);
+        return ResponseEntity.created(createdUri).body(event);
+    }
+
+}
