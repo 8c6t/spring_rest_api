b57755e4525f7c23309a545e8ec5d6d3f424822f
8c6t <nkw8620@gmail.com>
2019-12-15 00:54:06 +0900

    2.7 Bad Request 응답

--- src/main/java/com/hachicore/demoinflearnrestapi/events/EventController.java
+++ src/main/java/com/hachicore/demoinflearnrestapi/events/EventController.java
@@ -27,20 +27,20 @@ public class EventController {
     @PostMapping
     public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
         if (errors.hasErrors()) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.badRequest().body(errors);
         }
 
         eventValidator.validate(eventDto, errors);
         if (errors. hasErrors()) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.badRequest().body(errors);
         }
 
         Event event = modelMapper.map(eventDto, Event.class);
 
         Event newEvent = eventRepository.save(event);
-        URI createdUri = linkTo(EventController.class).slash("{id}").toUri();
+        URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
 
-        return ResponseEntity.created(createdUri).body(newEvent);
+        return ResponseEntity.created(createdUri).body(event);
     }
 
 }
