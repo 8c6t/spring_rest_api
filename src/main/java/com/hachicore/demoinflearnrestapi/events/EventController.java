c525e1268d823719bfe1b92fa46fcddb948f79b9
8c6t <nkw8620@gmail.com>
2019-12-15 23:20:58 +0900

    3.2 스프링 HATEOAS 적용
    Resource -> EntityModel

--- src/main/java/com/hachicore/demoinflearnrestapi/events/EventController.java
+++ src/main/java/com/hachicore/demoinflearnrestapi/events/EventController.java
@@ -3,6 +3,7 @@ package com.hachicore.demoinflearnrestapi.events;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -39,9 +40,14 @@ public class EventController {
         event.update();
         Event newEvent = eventRepository.save(event);
 
-        URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
+        WebMvcLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(newEvent.getId());
+        URI createdUri = selfLinkBuilder.toUri();
 
-        return ResponseEntity.created(createdUri).body(event);
+        EventResource eventResource = new EventResource(event);
+        eventResource.add(linkTo(EventController.class).withRel("query-events"));
+        eventResource.add(selfLinkBuilder.withRel("update-event"));
+
+        return ResponseEntity.created(createdUri).body(eventResource);
     }
 
 }
