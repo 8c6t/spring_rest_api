8d2736209f32e37377ad708f8ca203fee9dd2cd6
8c6t <nkw8620@gmail.com>
2019-12-18 12:49:31 +0900

    4.4 테스트 코드 리팩토링

--- src/test/java/com/hachicore/demoinflearnrestapi/index/IndexControllerTest.java
+++ src/test/java/com/hachicore/demoinflearnrestapi/index/IndexControllerTest.java
@@ -1,31 +1,13 @@
 package com.hachicore.demoinflearnrestapi.index;
 
-import com.hachicore.demoinflearnrestapi.common.RestDocsConfiguration;
+import com.hachicore.demoinflearnrestapi.common.BaseControllerTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocsConfiguration.class)
-@ActiveProfiles("test")
-public class IndexControllerTest {
-
-    @Autowired
-    MockMvc mockMvc;
+public class IndexControllerTest extends BaseControllerTest {
 
     @Test
     public void index() throws Exception {
@@ -33,7 +15,6 @@ public class IndexControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("_links.events").exists())
         ;
-
     }
 
 }
