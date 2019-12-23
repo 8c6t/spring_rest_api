2d6d0f452cf158ea2febf161cb3610ecb4348655
8c6t <nkw8620@gmail.com>
2019-12-23 11:03:15 +0900

    5.11 출력값 제한하기

--- src/main/java/com/hachicore/demoinflearnrestapi/events/Event.java
+++ src/main/java/com/hachicore/demoinflearnrestapi/events/Event.java
@@ -1,6 +1,8 @@
 package com.hachicore.demoinflearnrestapi.events;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.hachicore.demoinflearnrestapi.accounts.Account;
+import com.hachicore.demoinflearnrestapi.accounts.AccountSerializer;
 import lombok.*;
 
 import javax.persistence.*;
@@ -29,6 +31,7 @@ public class Event {
     private EventStatus eventStatus = EventStatus.DRAFT;
 
     @ManyToOne
+    @JsonSerialize(using = AccountSerializer.class)
     private Account manager;
 
     public void update() {
